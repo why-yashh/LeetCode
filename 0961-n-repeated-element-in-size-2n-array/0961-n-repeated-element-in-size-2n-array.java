914fcdfceead8c14434642c1d43515268f5e277f
YASH RAJ SINGH <148228646+why-yashh@users.noreply.github.com>
2026-01-02 22:24:49 +0530

    Time: 0 ms (100%), Space: 47.5 MB (50.87%) - LeetHub

---
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                return num;
+            }
+            seen.add(num);
+        }
+        return -1; // Unreachable per constraints
+    }
+}
