5ffaea0119181f46e4dd8894af5531a460024fd7
YASH RAJ SINGH <148228646+why-yashh@users.noreply.github.com>
2026-02-06 22:25:22 +0530

    Time: 7 ms (37.21%), Space: 45.5 MB (32.16%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int thirdMax(int[] nums) {
+        Arrays.sort(nums);
+        HashSet<Integer> s = new HashSet<>();
+
+        for(int i = nums.length-1; i >= 0; i--){
+            s.add(nums[i]);
+            if(s.size() == 3){
+                return nums[i];
+            }
+        }
+        return nums [nums.length-1];
+    }
+}
